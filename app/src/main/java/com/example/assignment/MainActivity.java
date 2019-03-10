7885f460f0eb33d3e110c57a35adfd7bd9de782b
leSon <31215388+hoanglethanhson@users.noreply.github.com>
2019-03-10 14:03:41 +0700

    Fix nav item id

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.shortPlans) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.longPlans) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.weather) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.trash) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.about) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.help) {
 
         }
 
