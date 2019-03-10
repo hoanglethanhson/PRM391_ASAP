a5123cfa2cb702e61fe9d9bf10eed32ae752972d
leSon <31215388+hoanglethanhson@users.noreply.github.com>
2019-03-10 15:09:39 +0700

    add db methods

--- app/src/main/java/com/example/assignment/DatabaseHandler.java
+++ app/src/main/java/com/example/assignment/DatabaseHandler.java
@@ -3,9 +3,12 @@ package com.example.assignment;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -104,11 +107,129 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //find a short term note
     public ShortTermNote findShortTermNote(String noteName) {
-        //TODO
+
         ShortTermNote note = new ShortTermNote();
         String query = "Select * From " + TABLE_SHORTNOTE
                 + " Where " + COLUMN_SHORTNOTE_TITLE + " = '" + noteName + "'";
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            note.setId(cursor.getInt(0));
+            note.setTitle(cursor.getString(1));
+            note.setContent(cursor.getString(2));
+            note.setDeadline(cursor.getColumnName(3));
+            note.setIsDeleted(cursor.getInt(4));
+            note.setLongNoteId(cursor.getInt(5));
 
+            return note;
+        }
+
+        return null;
+    }
+
+    //find a long term note
+    public LongTermNote findLongTermNote(String noteName) {
+        LongTermNote note = new LongTermNote();
+        String query = "Select * From " + TABLE_LONGNOTE
+                + " Where " + COLUMN_LONGNOTE_TITLE + " = '" + noteName + "'";
+
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            note.setId(cursor.getInt(0));
+            note.setTitle(cursor.getString(1));
+
+            return note;
+        }
+        return null;
+    }
+
+    //find short term plans of an long term plan
+    public ArrayList<ShortTermNote> findShortByLong(int longTermId) {
+        ShortTermNote note;
+        ArrayList<ShortTermNote> notes = new ArrayList<>();
+        String query = "Select * From " + TABLE_SHORTNOTE
+                + " Where " + COLUMN_SHORTNOTE_LONGNOTEID + " = " + String.valueOf(longTermId) + "";
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                note = new ShortTermNote();
+                note.setId(cursor.getInt(0));
+                note.setTitle(cursor.getString(1));
+                note.setContent(cursor.getString(2));
+                note.setDeadline(cursor.getColumnName(3));
+                note.setIsDeleted(cursor.getInt(4));
+                note.setLongNoteId(cursor.getInt(5));
+
+                notes.add(note);
+            } while (cursor.moveToNext());
+        }
+        return notes;
+    }
+
+    //delete a short term note
+    public int deleteShortTermNote(String noteName) {
+        ShortTermNote note = findShortTermNote(noteName);
+        if (note != null) {
+            SQLiteDatabase db = getWritableDatabase();
+            int result = db.delete(TABLE_SHORTNOTE, COLUMN_SHORTNOTE_ID + " = ?", new String[]{String.valueOf(note.getId())});
+            return  result;
+        }
+        return 0;
+    }
+
+    //delete a long term note
+    public int deleteLongTermNote(String noteName) {
+        LongTermNote note = findLongTermNote(noteName);
+        if (note != null) {
+            SQLiteDatabase db = getWritableDatabase();
+            int longResult = db.delete(TABLE_LONGNOTE, COLUMN_LONGNOTE_ID + " = ?", new String[]{String.valueOf(note.getId())});
+
+
+            ArrayList<ShortTermNote> shortNotes = findShortByLong(note.getId());
+            String [] shortNoteIds = new String[shortNotes.size()];
+            int i = 0;
+            for (ShortTermNote shortTermNote : shortNotes) {
+                shortNoteIds[i] = String.valueOf(shortTermNote.getId());
+                i++;
+            }
+
+            int shortResult = db.delete(TABLE_SHORTNOTE, COLUMN_SHORTNOTE_ID + " = ?", shortNoteIds);
+
+            return longResult;
+        }
+        return 0 ;
+    }
+
+    //find urgent notes
+    public ArrayList<ShortTermNote> findAllShortNotes() {
+        ShortTermNote note;
+        ArrayList<ShortTermNote> notes = new ArrayList<>();
+        String query = "Select * From " + TABLE_SHORTNOTE;
+
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                note = new ShortTermNote();
+                note.setId(cursor.getInt(0));
+                note.setTitle(cursor.getString(1));
+                note.setContent(cursor.getString(2));
+                note.setDeadline(cursor.getColumnName(3));
+                note.setIsDeleted(cursor.getInt(4));
+                note.setLongNoteId(cursor.getInt(5));
+
+                notes.add(note);
+            } while (cursor.moveToNext());
+        }
+        return notes;
+    }
+
+    //find urgent notes
+    public ArrayList<ShortTermNote> findUrgentNotes() {
+        //TODO
+        ArrayList<ShortTermNote> allNotes = findAllShortNotes();
         return null;
     }
 }
