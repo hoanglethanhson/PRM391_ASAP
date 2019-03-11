1438be266a59ae1ef44cd82e3fe4c6b0ea14abea
phanquanganh <phanquanganh61@gmail.com>
2019-03-12 00:19:34 +0700

    Add demo about page using available library

--- app/src/main/java/com/example/assignment/AboutActivity.java
+++ app/src/main/java/com/example/assignment/AboutActivity.java
@@ -2,12 +2,35 @@ package com.example.assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise here");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+
+                .setDescription("This is demo version")
+                .addItem(new Element().setTitle("Versiom 1.0"))
+                .addItem(adsElement)
+                .addGroup("Contact us:")
+                .addEmail("quangtungson1@gmail.com")
+                .addWebsite("www.dailymail.co.uk")
+                .addFacebook("seaboyhl")
+                .addInstagram("phanquanganh")
+                .addYoutube("Fancorn")
+                .addGitHub("hoanglethanhson/PRM391_ASAP")
+                .create();
+        setContentView(aboutPage);
+
     }
 }
